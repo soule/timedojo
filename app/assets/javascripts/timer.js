46067bddfea51cbeab737f67a99766a1aca3fb25
Souleiman Benhida <souleben@gmail.com>
2017-04-12 08:36:06 -0400

    Sorta works .. but signup doesnt work on production...

--- app/assets/javascripts/timer.js
+++ app/assets/javascripts/timer.js
@@ -3,4 +3,18 @@ $(function() {
   $("#reset").unbind().submit();
 
   $("#timer_controls").show();
+
+
+  $("#minutes").editable("click", function(e){
+	  updateLengthField();
+  });
+
+  $("#seconds").editable("click", function(e){
+	 updateLengthField();
+  });
+
 });
+
+function updateLengthField() {
+	$("#sessionlength").val(parseInt($("#minutes").text())*60 + parseInt($("#seconds").text()));
+}
