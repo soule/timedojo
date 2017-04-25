7971fd60bb6cfd134c3eaf64faf2f8ee98b71e33
Souleiman Benhida <souleben@gmail.com>
2017-04-25 11:06:20 -0400

    now using figaro properly

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,5 +5,5 @@ set :deploy_to, '/home/soule/worktracker'
 set :scm_username, "soule"
 set :scm_password, "soule25-"
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
