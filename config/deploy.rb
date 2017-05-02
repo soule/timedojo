f28d00f990d2d3061a56a81bbd2320c6c9394ec3
Souleiman Benhida <souleben@gmail.com>
2017-05-02 11:24:19 -0400

    reverting.. still protected'

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,6 +5,5 @@ set :deploy_to, '/home/soule/worktracker'
 set :scm_username, ENV['scm_username']
 set :scm_password, ENV['scm_password']
 
-append :linked_files, "config/database.yml", "config/secrets.yml"
-set :linked_files %w{config/application.yml}
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
