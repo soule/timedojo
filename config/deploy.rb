240701c1cee30b16fd9ed56e422d39103dd40cdd
Souleiman Benhida <souleben@gmail.com>
2017-05-02 11:15:52 -0400

    Trying to get application.yml to commit

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,8 +2,9 @@ set :application, 'worktracker'
 set :repo_url, 'https://github.com/soule/worktracker.git'
 
 set :deploy_to, '/home/soule/worktracker'
-set :scm_username, "soule"
-set :scm_password, "soule25-"
+set :scm_username, ENV['scm_username']
+set :scm_password, ENV['scm_password']
 
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
+#append :linked_files, "config/database.yml", "config/secrets.yml", "config/application.yml"
+set :linked_files %w(config/database.yml config/secrets.yml config/application.yml)
 append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/uploads"
