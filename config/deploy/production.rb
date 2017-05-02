1fae0dd268ff71a2f6be465ac858a621d6b61d9a
Souleiman Benhida <souleben@gmail.com>
2017-05-02 13:43:28 -0400

    using mailgun api

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -3,3 +3,8 @@ set :stage, :production
 # Replace 127.0.0.1 with your server's IP address!
 server '64.137.248.220', user: 'soule', roles: %w{web app}
 
+config.action_mailer.delivery_method = :mailgun
+config.action_mailer.mailgun_settings = {
+        api_key: 'key-2a1d80c84b04bbc0bb2956d6805f1169',
+        domain: 'mail.timedojo.com'
+}
